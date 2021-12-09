83f3486041474c68b7e67a7e4e11c383b21fb660
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-09 16:27:14 -0600

    Built out Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/layout/navbar'
+
 export default function App() {
 	return (
-		<div>
-			<h1>Hello world!</h1>
-		</div>
+		<Router>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+
+				<main>Content</main>
+			</div>
+		</Router>
 	)
 }
