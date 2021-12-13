54d0339793ba67354d0fb05c04815ae34db9662f
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-13 00:18:48 -0600

    Created UserList component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 export default function UserResults() {
-	return <div>User Results</div>
+	const [users, setUsers] = useState([])
+	const [loading, setLoading] = useState(true)
+
+	useEffect(() => {
+		fetchUsers()
+	}, [])
+
+	const fetchUsers = async () => {
+		const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+			headers: { Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}` },
+		})
+
+		const data = await response.json()
+
+		setUsers(data)
+		setLoading(false)
+	}
+
+	if (!loading) {
+		return (
+			<div className='user-results'>
+				{users.map((user) => (
+					<div className='user-result' key={user.id}>
+						<img src={user.avatar_url} alt={user.login} />
+						<a href={user.html_url}>{user.login}</a>
+					</div>
+				))}
+			</div>
+		)
+	} else {
+		return <h3>Loading...</h3>
+	}
 }
