f831c88b63fd113ba1f3b4c68170b49d65439012
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-17 18:06:02 -0600

    Built out Alert component and functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Alert from './components/layout/Alert'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 import { GithubProvider } from './context/github/GithubContext'
@@ -16,6 +17,7 @@ export default function App() {
 						<Navbar />
 
 						<main className='container mx-auto px-3 pb-12'>
+							<Alert />
 							<Routes>
 								<Route path='/' element={<Home />} />
 								<Route path='/about' element={<About />} />
