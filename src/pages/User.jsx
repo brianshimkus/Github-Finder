1649f73a73e76bce52e374dc65775d58dce8d37f
Brian Shimkus <brianshimkus1@gmail.com>
2021-12-22 23:40:05 -0600

    Moved SearchUsers to actions file

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -14,6 +14,7 @@ export default function User() {
 	useEffect(() => {
 		getUser(params.login)
 		getUserRepos(params.login)
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	const {
